a349a179702f725f803282a182aff074dfd73e91
Zer <livefatezz@gmail.com>
2024-10-05 14:46:18 +0800

    feat: modal when clicking on subjects at homepage

---
+++ frontend/src/pages/game/page.tsx
@@ -0,0 +1,9 @@
+const mockQuestion = {};
+
+const Game = () => {
+  return (
+    <div className="w-screen h-screen overflow-hidden items-center justify-center"></div>
+  );
+};
+
+export default Game;
